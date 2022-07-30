d7e881ffeb946316c135d71200e05bed0c599d18
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-30 12:27:34 +0530

    Multiple JSX element inside render()

--- 0. Basics/src/index.js
+++ 0. Basics/src/index.js
@@ -1,7 +1,11 @@
+const { Fragment } = require('react');
 const React = require('react');
 const ReactDOM = require('react-dom');
 
 ReactDOM.render(
-<h1>Hello World!!!</h1>,
-document.getElementById('root')
+            <Fragment>
+            <h1>Hello World!!!</h1>
+            <p>This is paragraph</p>
+            </Fragment>,
+            document.getElementById('root')
 );
