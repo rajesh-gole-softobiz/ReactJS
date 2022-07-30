67501ca951dd66a37d342ce61d1996f2cd0fae97
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-30 12:48:19 +0530

    JSX Expression in React - Template Literale

--- 0. Basics/src/index.js
+++ 0. Basics/src/index.js
@@ -2,21 +2,32 @@ const { Fragment } = require('react');
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-//##################################### 2. JSX Challenge-1
+//##################################### 3. JSX Expression in React - Template Literale
+const fname= 'RAJESH';
+const lname='GOLE';
 ReactDOM.render(
     <>
-    <h1>Top Companies</h1>
-    <p>List of 5 Best Companies</p>
-    <ol>
-        <li>Facebook</li>
-        <li>Amazon</li>
-        <li>Apple</li>
-        <li>Netflix</li>
-        <li>Goole</li>
-    </ol>
+    <h1>{`My full name is ${fname} ${lname}`}</h1>
     </>,
     document.getElementById('root')
 )
+
+
+//##################################### 2. JSX Challenge-1
+// ReactDOM.render(
+//     <>
+//     <h1>Top Companies</h1>
+//     <p>List of 5 Best Companies</p>
+//     <ol>
+//         <li>Facebook</li>
+//         <li>Amazon</li>
+//         <li>Apple</li>
+//         <li>Netflix</li>
+//         <li>Goole</li>
+//     </ol>
+//     </>,
+//     document.getElementById('root')
+// )
 
 //##################################### 1. Multiple JSX element inside render()
 // ReactDOM.render(
