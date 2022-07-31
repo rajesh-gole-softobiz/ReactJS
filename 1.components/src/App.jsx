e4f8a3f369217e52f3857c9186fe6d7bc2f002ab
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-31 17:10:19 +0530

    Simple Calculator App

--- 1.components/src/App.jsx
+++ 1.components/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Heading from './Heading';
 import Para from './Para';
+import {add,sub,mul,div} from './Calc';
+
 
 function App() {
     return (
         <>
         <Heading/>
         <Para/>
+        <p>Addition of 40 + 10 = {add(40,10)}</p>
+        <p>Subtraction of 40 - 10 = {sub(40,10)}</p>
+        <p>Multiplication of 40 * 10 = {mul(40,10)}</p>
+        <p>Division of 37 / 3 = {div(37,3)}</p>
         </>
     )
 }
