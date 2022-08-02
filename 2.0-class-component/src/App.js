71dee4416ffa6e1a025ad896b0c649be98a8544d
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-08-02 12:02:50 +0530

    Functional Component

---
+++ 2.0-class-component/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Card1 from "./components/Card1";
+
+function App() {
+    return <>
+    <h1>Hello</h1>
+    <Card1 name='card1' desc='this is card1'/>
+    </>
+}
+
+export default App;
---
+++ 2.0-class-component/src/components/Card1.js
@@ -0,0 +1,8 @@
+function Card1(props){
+    return <>
+    <h2>{props.name}</h2>
+    <h2>{props.desc}</h2>
+    </>
+}
+
+export default Card1;
