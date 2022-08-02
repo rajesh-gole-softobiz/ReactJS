a2f9cf77a6600e0078ccc15beb71227dad7fc4b3
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-08-02 11:24:03 +0530

    Mapping data to components

--- 2.props/src/App.jsx
+++ 2.props/src/App.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import Card from "./Card";
 
+//Mapping data to components
+import Data from './data.json';
+
 function App(){
+    //Method-3: Print JSON data with Map
+    let items = [];
+    items = Data.map((i,index) => <Card key={index} myTitle={i.title} myDesc={i.desc}/>)
+    // All Data will store to i 
+
+    //Method-2: Print JSON data with for loop
+    // let items = [];
+    // for(let x=0;x<Data.length; x++){
+    //     items.push( <Card myTitle={Data[x].title} myDesc={Data[x].desc}/> )
+    // }
+
     return <>
-    <Card myTitle='My first props'/>
-    <Card myTitle='My second props'/>
-    <Card myTitle='My third props'/>
+
+        {items}
+        {Data.map((i) => <Card myTitle={i.title} myDesc={i.desc}/>)}
+
+    {/* Method-1 */}
+    {/* <Card myTitle={Data[0].title} myDesc={Data[0].desc}/>
+    <Card myTitle={Data[1].title} myDesc={Data[1].desc}/>
+    <Card myTitle={Data[2].title} myDesc={Data[2].desc}/> */}
     </>
 }
 
--- 2.props/src/Card.jsx
+++ 2.props/src/Card.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-const myDesc = 'JSX is a syntax extension of JavaScript. It is used with React to describe what the user interface should look like. By using JSX, we can write HTML structures in the same file that contains JavaScript code.';
+// const myDesc = 'JSX is a syntax extension of JavaScript. It is used with React to describe what the user interface should look like. By using JSX, we can write HTML structures in the same file that contains JavaScript code.';
 const date = new Date();
 const myDate = date.toLocaleDateString();
 function Card(props) {
-    const {myTitle} = props;
+    const {myTitle,myDesc} = props;
     return <div className='card'>
     <h3 className='cardTitle'>{myTitle}</h3>
     <p className='cardDesc'>{myDesc}</p>
